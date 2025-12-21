c4ad59578de236c264e44dfa3d21dbfcd16b86a7
tanno <tanno@TSELITEBOOK>
2025-12-21 19:43:40 +0900

    プロジェクトファイルを追加。

---
+++ Sample1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sample1
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        // JIT Profile Optimization を有効にするためのコード
+        // JIT Profile を保存するディレクトリをユーザーのドキュメントフォルダに設定
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string profileRoot = System.IO.Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "JITProfiles");
+            if (!System.IO.Directory.Exists(profileRoot))
+            {
+                System.IO.Directory.CreateDirectory(profileRoot);
+            }
+            System.Runtime.ProfileOptimization.SetProfileRoot(profileRoot);
+            System.Runtime.ProfileOptimization.StartProfile("Sample1.jitprofile");
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Sample1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// MainWindow クラスの新しいインスタンスを初期化し、アプリケーションウィンドウの主要なデータコンテキストを設定します。
+        /// </summary>
+        /// <remarks>このコンストラクタは `DataContext` を `MainVM` の新しいインスタンスに設定し、ウィンドウのコントロールでデータバインディングを有効にします。WPF アプリケーションでメインウィンドウを作成する際にこのコンストラクタを使用します。</remarks>
+        public MainWindow()
+        {
+            // デザイナー生成コードを初期化
+            InitializeComponent();
+
+            // DataContext を MainVM の新しいインスタンスに設定
+            this.DataContext = new MainVM();
+        }
+
+    }
+}
