b0e372dbf56a19927b29daa21a5441743a3495eb
tscube <tscube450@gmail.com>
2026-01-19 00:05:34 +0900

    Version 2.3.0.0 に更新

--- SQLiteEditor/Properties/AssemblyInfo.cs
+++ SQLiteEditor/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
