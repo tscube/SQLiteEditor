b1e42ee7ee7691a8d0e18f72dc6c1feda0ee5056
tscube <tscube450@gmail.com>
2026-01-19 00:04:47 +0900

    バージョンアップ時に設定を引継ぐ処理を追加

--- SQLiteEditor/App.xaml.cs
+++ SQLiteEditor/App.xaml.cs
@@ -14,10 +14,26 @@ namespace SQLiteEditor
     /// </summary>
     public partial class App : Application
     {
-        // JIT Profile Optimization を有効にするためのコード
-        // JIT Profile を保存するディレクトリをユーザーのドキュメントフォルダに設定
+        /// <summary>
+        /// Handles application startup initialization, including settings upgrade and enabling JIT profile
+        /// optimization.
+        /// </summary>
+        /// <remarks>This method upgrades user settings if they have not been upgraded yet and configures
+        /// the JIT profile optimization to use a profile stored in the user's Documents folder. It should be called as
+        /// part of the application's startup sequence.</remarks>
+        /// <param name="e">An object that contains the event data for the startup event.</param>
         protected override void OnStartup( StartupEventArgs e )
         {
+            // まだUpgradeしていない場合のみ実行
+            if( !SQLiteEditor.Properties.Settings.Default.IsUpgraded )
+            {
+                SQLiteEditor.Properties.Settings.Default.Upgrade();
+                SQLiteEditor.Properties.Settings.Default.IsUpgraded = true;
+                SQLiteEditor.Properties.Settings.Default.Save();
+            }
+
+            // JIT Profile Optimization を有効にするためのコード
+            // JIT Profile を保存するディレクトリをユーザーのドキュメントフォルダに設定
             string appName = Assembly.GetExecutingAssembly().GetName().Name;
             string appRoot = System.IO.Path.Combine( Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), appName);
             if( !System.IO.Directory.Exists( appRoot ) )
@@ -28,5 +44,8 @@ namespace SQLiteEditor
             System.Runtime.ProfileOptimization.StartProfile( appName + ".jitprofile" );
             base.OnStartup( e );
         }
+
+        // Properties.Settings.Defaultの古いバージョンのデータを移行する
+
     }
 }
--- SQLiteEditor/Properties/Settings.Designer.cs
+++ SQLiteEditor/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace SQLiteEditor.Properties {
                 this["DbFilePath"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IsUpgraded {
+            get {
+                return ((bool)(this["IsUpgraded"]));
+            }
+            set {
+                this["IsUpgraded"] = value;
+            }
+        }
     }
 }
